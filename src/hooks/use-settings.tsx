650e25c910173fa23bfe419bcdd9fa08a4cedce3
Reality Design <realitydesign5561@gmail.com>
2026-08-05 12:04:22 +0100

    Update use-settings.tsx

--- src/hooks/use-settings.tsx
+++ src/hooks/use-settings.tsx
@@ -1,9 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { defaultSettings, fetchSettings, type SiteSettings } from "@/lib/settings";
 
-import { useQuery } from "@tanstack/react-query";
-import { defaultSettings, fetchSettings, type SiteSettings } from "@/lib/settings";
-
 export function useSettings(): SiteSettings {
   const { data } = useQuery({
     queryKey: ["website-settings"],
