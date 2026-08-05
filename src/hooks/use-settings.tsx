1203435cf8837e06a299a2c009af78678f3c0760
Reality Design <realitydesign5561@gmail.com>
2026-08-05 12:02:54 +0100

    Update use-settings.tsx

--- src/hooks/use-settings.tsx
+++ src/hooks/use-settings.tsx
@@ -1,11 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import { defaultSettings, fetchSettings, type SiteSettings } from "@/lib/settings";
 
+import { useQuery } from "@tanstack/react-query";
+import { defaultSettings, fetchSettings, type SiteSettings } from "@/lib/settings";
+
 export function useSettings(): SiteSettings {
   const { data } = useQuery({
     queryKey: ["website-settings"],
     queryFn: fetchSettings,
-    staleTime: 60_000,
+    staleTime: 0,
+    refetchOnWindowFocus: true,
   });
+
   return data ?? defaultSettings;
 }
