d952a4fb8b61d98595d91c02c2459d1fbd8f9b62
Reality Design <realitydesign5561@gmail.com>
2026-08-07 08:27:37 +0100

    Update properties.ts

--- src/lib/properties.ts
+++ src/lib/properties.ts
@@ -191,3 +191,6 @@ export function slugify(value: string) {
     .replace(/[^a-z0-9]+/g, "-")
     .replace(/^-|-$/g, "")}-${Math.random().toString(36).slice(2, 7)}`;
 }
+
+export const whatsappLink =
+  "https://wa.me/2349117511768";
