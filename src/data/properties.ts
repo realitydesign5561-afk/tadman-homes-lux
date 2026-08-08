d8e3a4f04c3455f961693a1c2dbf530846f1fb43
Reality Design <realitydesign5561@gmail.com>
2026-08-08 03:38:54 -0700

    chore(images): remove stock property thumbnails; export only hero asset for fallback

--- src/data/properties.ts
+++ src/data/properties.ts
@@ -1,9 +1,6 @@
-const prop1 = "https://images.pexels.com/photos/8089172/pexels-photo-8089172.jpeg?auto=compress&cs=tinysrgb&w=900&h=700";
-const prop2 = "https://images.pexels.com/photos/8146330/pexels-photo-8146330.jpeg?auto=compress&cs=tinysrgb&w=900&h=700";
-const prop3 = "https://images.pexels.com/photos/8146332/pexels-photo-8146332.jpeg?auto=compress&cs=tinysrgb&w=900&h=700";
-const hero = "https://images.pexels.com/photos/28054849/pexels-photo-28054849.jpeg?auto=compress&cs=tinysrgb&w=1400&h=1000";
+import heroVilla from "@/assets/hero-villa.jpg";
 
-export const propertyImages = { prop1, prop2, prop3, hero };
+export const propertyImages = { hero: heroVilla };
 
 export type { Property } from "@/lib/properties";
 
